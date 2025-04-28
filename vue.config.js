c4df904a5ff6e9c9c87c12e4c1f9eca67b5996fd
Fatimah Alzahrani <12fatimah.15@gmail.com>
2025-04-28 08:10:43 +0300

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   publicPath: "/",
   outputDir: "dist",
+  assetsDir: "assets",
+  productionSourceMap: false,
+  configureWebpack: {
+    performance: {
+      hints: false,
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  },
 };
